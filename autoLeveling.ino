347a62b5a6d4a88f16b3d065fe46021bff83414a
Neddys0404 <tepxcube@gmail.com>
2025-01-01 15:25:02 +0800

    v2 major update, removed struct method and all hard vars declared in global var

--- autoLeveling.ino
+++ autoLeveling.ino
@@ -1,4 +1,3 @@
-#include <Wire.h>
 #include <Servo.h>
 #include <Adafruit_VL53L0X.h>  
 
@@ -22,143 +21,259 @@ int s2_ang = init_angle;
 int s3_ang = init_angle;
 
 //for tof sensors
-Adafruit_VL53L0X tof_1;
-Adafruit_VL53L0X tof_2;
-Adafruit_VL53L0X tof_3;
+#define TOF_0_ADDRESS 0x30
+#define TOF_1_ADDRESS 0x31
+#define TOF_2_ADDRESS 0x32
 
-typedef struct {
+#define TOF_0_XSHT 4
+#define TOF_1_XSHT 6
+#define TOF_2_XSHT 8
 
-  Adafruit_VL53L0X *psensor; // pointer to object
-  TwoWire *pwire;
-  int id;                                                     // I2C id number for the sensor
-  int shutdown_pin;                                           // which pin for shutdown;
-  int interrupt_pin;                                          // which pin to use for interrupts.
-  Adafruit_VL53L0X::VL53L0X_Sense_config_t sensor_config;     // options for how to use the sensor
-  uint16_t range;                                             // range value used in continuous mode stuff.
-  uint8_t sensor_status;                                      // status from last ranging in continuous.
-  
-} tofObj;
+#define COUNT_TOFS 3
 
+Adafruit_VL53L0X tof_0 = Adafruit_VL53L0X();
+Adafruit_VL53L0X tof_1 = Adafruit_VL53L0X();
+Adafruit_VL53L0X tof_2 = Adafruit_VL53L0X();
 
-tofObj tofArr[] = {
+VL53L0X_RangingMeasurementData_t tof_0_reading;
+VL53L0X_RangingMeasurementData_t tof_1_reading;
+VL53L0X_RangingMeasurementData_t tof_2_reading;
 
-  {&tof_1, &Wire, 0x30, 4, 5, Adafruit_VL53L0X::VL53L0X_SENSE_DEFAULT, 0, 0},
-  {&tof_2, &Wire, 0x31, 6, 7, Adafruit_VL53L0X::VL53L0X_SENSE_DEFAULT, 0, 0},
-  {&tof_3, &Wire, 0x32, 8, 9, Adafruit_VL53L0X::VL53L0X_SENSE_DEFAULT, 0, 0}
-  
-};
-
-const int COUNT_TOFS = sizeof(tofArr) / sizeof(tofArr[0]);
-
-uint16_t dists[COUNT_TOFS];
+int dists[COUNT_TOFS];
 
 void init_tofSensors() {
-  
-  bool sensorFound = false;
-  
-  // Set all shutdown pins low to shutdown sensors
-  for (int i = 0; i < COUNT_TOFS; i++) {
-    
-    digitalWrite(tofArr[i].shutdown_pin, LOW);
-    delay(10); //buffer
-    
-  }
 
-  for (int i = 0; i < COUNT_TOFS; i++) {
-    
-    digitalWrite(tofArr[i].shutdown_pin, HIGH);
-    delay(10); //buffer
-    
-    if (tofArr[i].psensor -> begin(tofArr[i].id, false, tofArr[i].pwire, tofArr[i].sensor_config)) {
-      
-      sensorFound = true;
-      
-    } else {
+  // reset all tofs
+  digitalWrite(TOF_0_XSHT, LOW);    
+  digitalWrite(TOF_1_XSHT, LOW);
+  digitalWrite(TOF_2_XSHT, LOW);
+  delay(10);
 
-      // for debug purpose
-      //Serial.print(i, DEC);
-      //Serial.print(" have no sensor.");
-      //Serial.println("");
-      
-    }
-    
+  // enable all tofs
+  digitalWrite(TOF_0_XSHT, HIGH);    
+  digitalWrite(TOF_1_XSHT, HIGH);
+  digitalWrite(TOF_2_XSHT, HIGH);
+  delay(10);
+
+  // configure all tofs one by one
+  digitalWrite(TOF_0_XSHT, HIGH);
+  digitalWrite(TOF_1_XSHT, LOW);
+  digitalWrite(TOF_2_XSHT, LOW);
+
+  if (!tof_0.begin(TOF_0_ADDRESS)) {
+
+    while( 1) { Serial.println("Failed to boot first VL53L0X."); delay(500);};
+
   }
+  delay(10);
+
+  digitalWrite(TOF_1_XSHT, HIGH);
+  digitalWrite(TOF_2_XSHT, LOW);
   
-  if (!sensorFound) {
-    
-    Serial.println("No valid sensors found");
-    while (1) {};
-    
+  if (!tof_1.begin(TOF_1_ADDRESS)) {
+
+    while(1) { Serial.println("Failed to boot second VL53L0X."); delay(500);};
+
+  }
+  delay(10);
+
+  digitalWrite(TOF_2_XSHT, HIGH);
+  
+  if (!tof_2.begin(TOF_2_ADDRESS)) {
+
+    while(1) { Serial.println("Failed to boot third VL53L0X."); delay(500);};
+
   }
   
 }
 
+//read distance once for all tof
+void read_distance() {
+
+  //read first tof
+  tof_0.rangingTest(&tof_0_reading, false); // pass in 'true' to get debug data printout!
+
+  /*
+  //for debug purpose
+  Serial.print("ToF at address 0x");
+  Serial.print(TOF_0_ADDRESS, HEX);
+  Serial.print(": ");
+
+  if (tof_0_reading.RangeStatus != 4) {
+
+    Serial.print(tof_0_reading.RangeMilliMeter, DEC);
+    Serial.print(" mm     ");
+
+  }
+  else {
+
+    Serial.print("Out of Range");
+
+  }
+  */
+
+  //read second tof
+  tof_1.rangingTest(&tof_1_reading, false); // pass in 'true' to get debug data printout!
+
+  /*
+  //for debug purpose
+  Serial.print("ToF at address 0x");
+  Serial.print(TOF_1_ADDRESS, HEX);
+  Serial.print(": ");
+
+  if (tof_1_reading.RangeStatus != 4) {
+
+    Serial.print(tof_1_reading.RangeMilliMeter, DEC);
+    Serial.print(" mm     ");
+
+  }
+  else {
+
+    Serial.print("Out of Range");
+
+  }
+  */
+
+  //read third tof
+  tof_2.rangingTest(&tof_2_reading, false); // pass in 'true' to get debug data printout!
+
+  /*
+  //for debug purpose
+  Serial.print("ToF at address 0x");
+  Serial.print(TOF_2_ADDRESS, HEX);
+  Serial.print(": ");
+
+  if (tof_2_reading.RangeStatus != 4) {
+
+    Serial.print(tof_2_reading.RangeMilliMeter, DEC);
+    Serial.print(" mm     ");
+
+  }
+  else {
+
+    Serial.print("Out of Range");
+
+  }
+
+  Serial.println();
+  */
+
+}
+
 //for control system's target
-uint16_t distInMm = 96;
+uint16_t distInMm = 69; //target, should be changed
 uint16_t errInMm[COUNT_TOFS];
-uint16_t tmpSumDist;
+uint16_t tmpSumDist[COUNT_TOFS];
 uint8_t aveCount = 50;
 bool r1_aligned;
 bool r2_aligned;
 bool r3_aligned;
 bool aligned;
 
-void checkAlignment(int index) {
+void checkAlignment(bool isTest) {
 
-  if (index >= 0 && index <= 2) {
+  tmpSumDist[0] = 0;
+  tmpSumDist[1] = 0;
+  tmpSumDist[2] = 0;
 
-    tmpSumDist = 0;
-  
-    //average to get consistent result.
-    for (int j = 0; j < aveCount; j++) {
-  
-      tmpSumDist += tofArr[index].psensor->readRange();
-      delay(20);
-        
+  //average to get consistent result.
+  for (int i = 0; i < aveCount; i++) {
+
+    read_distance();
+    delay(10);
+
+    if (tof_0_reading.RangeStatus != 4) {
+
+      tmpSumDist[0] += tof_0_reading.RangeMilliMeter;
+      
     }
-    dists[index] = tmpSumDist/aveCount;
-    errInMm[index] = distInMm - dists[index];
-    
+    else {
+      /* Debug */
+    }
+
+    if (tof_1_reading.RangeStatus != 4) {
+
+      tmpSumDist[1] += tof_1_reading.RangeMilliMeter;
+      
+    }
+    else {
+      /* Debug */
+    }
+
+    if (tof_2_reading.RangeStatus != 4) {
+
+      tmpSumDist[2] += tof_2_reading.RangeMilliMeter;
+      
+    }
+    else {
+      /* Debug */
+    }
+        
+  }
+
+  for (int i = 0; i < COUNT_TOFS; i++) {
+
+    dists[i] = tmpSumDist[i]/aveCount;
+    errInMm[i] = distInMm - dists[i];
+
+  }
+
+  if (isTest) {
+
+    Serial.print("Test measure invoked!");
+    Serial.println();
+    Serial.println();
+
+    Serial.print("At address 0x");
+    Serial.print(TOF_0_ADDRESS, HEX);
+    Serial.print("Current Reading: ");
+    Serial.print(dists[0], DEC);
+    Serial.println();
+    Serial.print("Error in mm: ");
+    Serial.print(errInMm[0], DEC);
+    Serial.println();
+
+    Serial.print("At address 0x");
+    Serial.print(TOF_1_ADDRESS, HEX);
+    Serial.print("Current Reading: ");
+    Serial.print(dists[1], DEC);
+    Serial.println();
+    Serial.print("Error in mm: ");
+    Serial.print(errInMm[1], DEC);
+    Serial.println();
+
+    Serial.print("At address 0x");
+    Serial.print(TOF_2_ADDRESS, HEX);
+    Serial.print("Current Reading: ");
+    Serial.print(dists[2], DEC);
+    Serial.println();
+    Serial.print("Error in mm: ");
+    Serial.print(errInMm[2], DEC);
+    Serial.println();
+
   }
   else {
 
-    for (int i = 0; i < COUNT_TOFS; i++) {
-  
-      tmpSumDist = 0;
-  
-      //average to get consistent result.
-      for (int j = 0; j < aveCount; j++) {
-  
-        tmpSumDist += tofArr[i].psensor->readRange();
-        delay(20);
-        
-      }
-      dists[i] = tmpSumDist/aveCount;
-      errInMm[i] = distInMm - dists[i];
-        
-    }
-      
-  }
+    r1_aligned = (errInMm[0] < 0.5 && errInMm[0] > -0.5);
+    r2_aligned = (errInMm[1] < 0.5 && errInMm[1] > -0.5);
+    r3_aligned = (errInMm[2] < 0.5 && errInMm[2] > -0.5);
+    aligned = ((r1_aligned && r2_aligned) && r3_aligned);
 
-  r1_aligned = (errInMm[0] < 0.5 && errInMm[0] > -0.5);
-  r2_aligned = (errInMm[1] < 0.5 && errInMm[1] > -0.5);
-  r3_aligned = (errInMm[2] < 0.5 && errInMm[2] > -0.5);
-  aligned = ((r1_aligned && r2_aligned) && r3_aligned);
+  }
   
 }
 
 
 // for dev mode, uncomment to use com terminal, comment to save bytes.
-//String command = "";
+String command = "";
 
 void setup() {
   // Enable Serial Comm
-  Serial.begin(9600);
-  while (!Serial && (millis() < 3000)) {};
-  Serial.setTimeout(1000);
+  Serial.begin(115200);
+  while (!Serial) { delay(1); }
 
-  // Enable I2C
-  Wire.begin();
+  Serial.println("Serial succesfully opened.");
+  delay(500);
 
   // Init Servos
   Servo_1.attach(SERVO_1);
@@ -170,23 +285,29 @@ void setup() {
   Servo_3.write(s3_ang);
 
   // Init ToF Sensors
-  for (int i = 0; i < COUNT_TOFS; i++) {
-    
-    pinMode(tofArr[i].shutdown_pin, OUTPUT);
-    digitalWrite(tofArr[i].shutdown_pin, LOW);
+  pinMode(TOF_0_XSHT, OUTPUT);
+  pinMode(TOF_1_XSHT, OUTPUT);
+  pinMode(TOF_2_XSHT, OUTPUT);
+  digitalWrite(TOF_0_XSHT, LOW);
+  digitalWrite(TOF_1_XSHT, LOW);
+  digitalWrite(TOF_2_XSHT, LOW);
 
-    if (tofArr[i].interrupt_pin >= 0) {
-      
-      pinMode(tofArr[i].interrupt_pin, INPUT_PULLUP);
-      
-    }
-      
-  }
   init_tofSensors();
+
+  Serial.println("Initilization Complete! Starting...");
+  Serial.println();
+  for (int i = 0; i < 10; i++) {
+
+    Serial.println(".");
+    delay(200);
+
+  }
 
 }
 
 void loop() {
+  //control system
+  /*
   checkAlignment(3);
 
   //control system
@@ -309,52 +430,12 @@ void loop() {
     delay(800);
     
   }
-  
-  // below codes are for dev mode, uncomment to use com terminal, comment to save bytes.
-  /*
-  
-  for (int i = 0; i < COUNT_TOFS; i++) {
-      
-    Serial.print("Sensor #");
-    Serial.print(i, DEC);
-      
-    Serial.print(" at IIC address 0x");
-    Serial.print(sensors[i].id, HEX);
-    Serial.print(": ");
-      
-    Serial.print(dists[i], DEC);
-    Serial.print(" mm.      ");
-    
-  }
-  Serial.println();
+  */
+
+  // below codes are for dev mode, uncomment to use terminal.
+  //periodically reads tof distance
    
-  if (Serial.available()) {
+  checkAlignment(true);
+  delay(1000);
 
-    command = Serial.readString();
-    //Serial.print("String: ");
-    //Serial.println(command);
-
-    s1_ang = command.substring(0,3).toInt();
-    s2_ang = command.substring(4,7).toInt();
-    s3_ang = command.substring(8,11).toInt();
-
-    Serial.println("\n New Servo Angle: ");
-    Serial.print("Servo 1: ");
-    Serial.println(s1_ang);
-    Serial.print("Servo 2: ");
-    Serial.println(s2_ang);\
-    Serial.print("Servo 3: ");
-    Serial.println(s3_ang);
-
-    // clear the string for new command input
-    command = ""; 
-
-    Servo_1.write(s1_ang);
-    delay(500);
-    Servo_2.write(s2_ang);
-    delay(500);
-    Servo_3.write(s3_ang);
-    delay(500); 
-    */
-    
 }
